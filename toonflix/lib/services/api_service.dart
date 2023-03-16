7598941bc8926a7633b61a384b364b2eb24b8d82
codecalf247 <89695621+codecalf247@users.noreply.github.com>
2023-03-16 22:59:41 +0900

    Start

---
+++ toonflix/lib/services/api_service.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
+  final String today = 'today';
+
+  // await : 결과가 올 때까지 기다림
+  // await를 쓰고 싶으면 async를 써야됨
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseUrl/$today');
+    await http.get(url);
+  }
+}
