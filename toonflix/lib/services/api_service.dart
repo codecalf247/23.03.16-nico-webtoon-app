94d0199fa17d84a366066e682a3e5117bb4754ef
codecalf247 <89695621+codecalf247@users.noreply.github.com>
2023-03-17 23:12:13 +0900

    Add webtoon_model % Decode Json

--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
@@ -6,12 +9,16 @@ class ApiService {
 
   // await : 결과가 올 때까지 기다림
   // await를 쓰고 싶으면 async를 써야됨
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final respones = await http.get(url);
     if (respones.statusCode == 200) {
-      print(respones.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(respones.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstances;
     }
     throw Error();
   }
